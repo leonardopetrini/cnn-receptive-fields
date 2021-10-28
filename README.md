b1f3a8633111c5d70d92f38866c267b6bbcfb311
Leonardo Petrini <leonardo.petrini@epfl.ch>
2021-10-28 11:26:02 +0000

    compute receptive fields centers

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ Receptive field computation for any CNN in PyTorch.
 
 The *receptive field* of a neuron is defined as the region of the input that can change the output of such a neuron. 
 
-The contribution to the value of neuron `n` of pixel `(i, j)` in an input image $x$ is computed by 
+The contribution to the value of neuron `n` of pixel `(i, j)` in an input image `x` is computed by 
 - setting all the weights in the network to constant values
 - replacing Max-Pooling by Average Pooling
 - switching off batch norm
-- switching on pixels in the input one at a time and see to which neurons they contribute. 
+- switching on pixels in the input one at a time to check responding neurons. 
 - contributions are then rescaled to be in [0, 1]
 
 The resulting receptive fields are a set of images `R^l = [R_n^l | n is a neuron in layer l]` that have the same size of the input. 
@@ -15,6 +15,7 @@ The resulting receptive fields are a set of images `R^l = [R_n^l | n is a neuron
 The value of `R^l_n[i, j] \in [0, 1]` corresponds to how much pixel `(i, j)`, in an input image - i.e. `x[:, i, j]` - would contribute to the output of neuron `n`, which lives in layer `l`, if the network had constant weights. 
 
 ### Install
+(this is for the `receptivefield` functions only)
 ```
 python -m pip install git+https://github.com/leonardopetrini/cnn-receptive-fields.git
 ```
@@ -27,3 +28,4 @@ For the example
 - `torchvision`
 - `matplotlib`
 
+cf. `requirements.txt` for versions.
--- receptivefield/__init__.py
+++ receptivefield/__init__.py
@@ -6,17 +6,17 @@ import torch
 import torch.nn as nn
 
 def receptive_field(f, n=32, ch=3):
-    '''
+    """
     Compute the receptive field of `f`, usually the network function up to a certain layer.
     It does so by setting all the weights to a constant, replacing max-pooling by avg-pooling and deactivating batch-norm.
-    
+
     :param torch.nn.Module f: network layer function
     :param int n: input size is n x n x ch
-    :param int ch: input channels 
+    :param int ch: input channels
     :return torch.Tensor: receptive field (as an n x n image) for each neuron in the activation map of layer f. Values of receptive_field are in [0, 1].
         Shape: [activation_map_size, activation_map_size, n, n]
 
-    '''
+    """
 
     constant_weights(f)
     replace_pooling(f)
@@ -30,13 +30,25 @@ def receptive_field(f, n=32, ch=3):
     return out.reshape(m, m, n, n)
 
 
+def receptive_field_center(rf):
+    """
+    :param torch.Tensor rf: receptive field for each neuron
+        Shape: [number_neurons, n, n]
+    :returns torch.Tensor: receptive fields centers
+        Shape: [number_neurons, 2]
+    """
+    n = rf.shape[-1]
+    X = torch.meshgrid(torch.arange(n, device=rf.device), torch.arange(n, device=rf.device))
+    return torch.stack([torch.nanmean((rf * X[i]).sum(dim=2) / rf.sum(dim=2), dim=1) for i in [1, 0]]).t()
+
+
 def constant_weights(model):
-    '''
+    """
     Set all weights to a constant and biases to zero in `model`.
     Deactivate batch norm.
     :param torch.nn.Module model: neural network model written in PyTorch.
     :return None: in-place function.
-    '''
+    """
     for module in model.modules():
         if isinstance(module, torch.nn.modules.BatchNorm2d):
             module = module.eval()
@@ -50,10 +62,10 @@ def constant_weights(model):
         
         
 def replace_pooling(m, name=None):
-    '''
+    """
     Replace MaxPool2d by AvgPool2d in `m`.
     :param torch.nn.Module m: neural network model written in PyTorch.
-    '''
+    """
     for attr_str in dir(m):
         target_attr = getattr(m, attr_str)
         if target_attr == torch.nn.MaxPool2d:
@@ -66,10 +78,10 @@ def replace_pooling(m, name=None):
         
         
 def one_pixel_inputs(n=32):
-    '''
+    """
     :param int n: input image side size (image is n x n)
-    :return torch.Tensor: a batch of n^2 images such that `imgs[i]` has the i-th pixel on (=1) and the rest is zero. 
-    '''
+    :return torch.Tensor: a batch of n^2 images such that `imgs[i]` has the i-th pixel on (=1) and the rest is zero.
+    """
     x = torch.zeros(n, n, n, n)
     for i in range(n):
         for j in range(n):
